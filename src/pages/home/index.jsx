5db6f6683cf042e70d055e95048bf34326a84621
adrianor85 <adrianorosa085@gmail.com>
2023-03-13 15:55:18 -0300

    starting in React

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export function Home() {
+  return (
+      <div className='container'>
+        <h1>Lista de Pressença</h1>
+        <input type="text" placeholder="Digite o nome..." />
+        <button>Adicionar</button>
+      </div>
+  )
+}
+
